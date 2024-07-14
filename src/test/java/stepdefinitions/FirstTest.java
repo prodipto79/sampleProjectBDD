1ffe608efbc63f2574a9614ac58c43d1b5477416
prodipto79 <prodiptodutta4@gmail.com>
2024-07-14 12:16:21 +0530

    firstprogram feature file

--- src/test/java/stepdefinitions/FirstTest.java
+++ src/test/java/stepdefinitions/FirstTest.java
@@ -31,6 +31,7 @@ public class FirstTest {
     @And("log off button displayed")
     public void logOffButtonDisplayed() {
         System.out.println("Log off button displayed");
+        System.out.println("Log off button not displayed - user1");
     }
 
     @When("^user login into application as user (.+) and password as (.+)$")
